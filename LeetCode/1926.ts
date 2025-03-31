6366b347effd9ee619c60361c21c4c4cf52c49c5
1toldyou <86680163+1toldyou@users.noreply.github.com>
2025-03-31 00:38:22 -0700

    working 1926

--- LeetCode/1926.ts
+++ LeetCode/1926.ts
@@ -1,21 +1,66 @@
-let globalBest = Number.MAX_SAFE_INTEGER; // need reset on every run;
-
 function nearestExit(maze: string[][], entrance: number[]): number {
-    globalBest = Number.MAX_SAFE_INTEGER;
-    let coord: Coord = {x: entrance[0], y: entrance[1]};
+    const isWall = (coord) => {
+        try {
+            return maze[coord.x][coord.y] === "+";
+        } catch (e) {
+            return false;
+        }
+    };
+    const canGo = (coord) => {
+        return !(coord.x < 0 || coord.x >= maze.length || coord.y < 0 || coord.y >= maze[0].length || isWall(coord));
+    };
+    const isExit = (coord) => {
+        if (!canGo(coord)){
+            // console.log(coord, "cannot be exit as it cannot be reached");
+            return false;
+        }
+        return coord.x === 0 || coord.x === maze.length-1 || coord.y === 0 || coord.y === maze[0].length-1;
+    };
 
-    // let depth = Number.MAX_SAFE_INTEGER;
-    let options = [
-        findPath(maze, {x: coord.x, y: coord.y+1}, [coord], 0),
-        findPath(maze, {x: coord.x, y: coord.y-1}, [coord], 0),
-        findPath(maze, {x: coord.x+1, y: coord.y}, [coord], 0),
-        findPath(maze, {x: coord.x-1, y: coord.y}, [coord], 0),
-    ].filter((x) => x > 0);
+    let visited: boolean[][] = [];
+    for (let i = 0; i < maze.length; i++){
+        visited.push((new Array(maze[i].length)).fill(false));
+    };
+    visited[entrance[0]][entrance[1]] = true;
+    let emptyCell = count2D(maze, "."); // including entrance
+    let visitedCell = 1;
 
-    if (options.length === 0){
-        return -1;
+    let currentLevel = [toCoord(entrance)];
+    let steps = 1;
+    while (currentLevel.length > 0){
+        let nextLevel = [];
+        console.log("currentLevel", currentLevel)
+        for (let coord of currentLevel){
+            let adjacents = [
+                {x: coord.x, y: coord.y+1}, 
+                {x: coord.x, y: coord.y-1},
+                {x: coord.x+1, y: coord.y},
+                {x: coord.x-1, y: coord.y},
+            ].filter((next) => {
+                if (!canGo(next)){ // will check if it out of bounds
+                    return false;
+                }
+
+                if (visited[next.x][next.y] === true){
+                    return false;
+                }
+
+                return true;
+            });
+
+            for (let next of adjacents){
+                if (isExit(next)){
+                    return steps;
+                }
+                nextLevel.push(next);
+                visited[next.x][next.y] = true
+            }
+        }
+        currentLevel = nextLevel;
+        steps++;
+        visitedCell = count2D(visited, true);
     }
-    return Math.min(...options);
+    return -1;
 };
 
 type Coord = {
@@ -23,111 +68,15 @@ type Coord = {
     y: number
 }
 
-
-
-function isExit(maze: string[][], coord: Coord){
-    if (!canGo(maze, coord)){
-        // console.log(coord, "cannot be exit as it cannot be reached");
-        return false;
-    }
-
-    return coord.x === 0 || coord.x === maze.length-1 || coord.y === 0 || coord.y === maze[0].length-1;
-}
-
-function isWall(maze: string[][], coord: Coord): boolean {
-    try {
-        return maze[coord.x][coord.y] === "+";
-    } catch (e) {
-        console.log("isWall() bad coordinate", coord)
-        return false;
+function toCoord(arr: number[]): Coord {
+    return {
+        x: arr[0],
+        y: arr[1],
     }
 }
 
-function canGo(maze: string[][], coord: Coord){
-    return !(coord.x < 0 || coord.x >= maze.length || coord.y < 0 || coord.y >= maze[0].length || isWall(maze, coord));
-}
-
-function findPath(maze: string[][], coord: Coord, previousPath: Coord[], depth: number): number {
-    depth++;
-    if (depth > globalBest){
-        console.log(`better solution ${globalBest} already exist`);
-        return -1;
-    }
-
-    // console.log("findPath()", coord);
-    if (!canGo(maze, coord)){
-        // console.log("findPath() cannot go", coord);
-        return -1;
-    }
-    if (isExit(maze, coord)){
-        globalBest = Math.min(globalBest, depth);
-        console.log("findPath()", coord, "is exit", `globalBest=${globalBest}`);
-        return 1;
-    }
-
-    // let up = ;
-    // let down = ;
-    // let left = ;
-    // let right = ;
-
-    let validDirections = [
-        {x: coord.x, y: coord.y+1}, 
-        {x: coord.x, y: coord.y-1},
-        {x: coord.x+1, y: coord.y},
-        {x: coord.x-1, y: coord.y},
-    ].filter((next) => !beenTo(next, previousPath));
-    // console.log("findPath()", coord, "validDirections", validDirections);
-
-    for (let next of validDirections){
-        if (isExit(maze, next)){
-            return 2;
-        }
-    }
-
-    let outcomes = [];
-    for (let next of validDirections){
-        outcomes.push(findPath(maze, next, [...previousPath, coord], depth));
-    }
-    // console.log("outcomes", outcomes);
-    outcomes = outcomes.filter((x) => x >= 0);
-    if (outcomes.length === 0){
-        return -1
-    }
-    return 1 + Math.min(...outcomes);
-}
-
-function beenTo(coord: Coord, previousPath: Coord[]): boolean {
-    for (let previousCoord of previousPath){
-        if (objectsAreEqual(coord, previousCoord)){
-            return true;
-        }
-    }
-    return false;
-}
-
-function arraysAreEqual(arr1, arr2) {
-    return JSON.stringify(arr1) === JSON.stringify(arr2);
-}
-
-function objectsAreEqual(obj1, obj2) {
-    // If they are not the same type, they are not equal
-    if (typeof obj1 !== 'object' || typeof obj2 !== 'object' || obj1 === null || obj2 === null) {
-        return false;
-    }
-
-    // Compare the number of keys in both objects
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-    if (keys1.length !== keys2.length) {
-        return false;
-    }
-
-    // Compare the values for each key
-    for (let key of keys1) {
-        if (obj1[key] !== obj2[key]) {
-            return false;
-        }
-    }
-
-    return true;
+function count2D(twoDArr: any[][], target): number{
+    return twoDArr.reduce((accu, row) => {
+        return accu += row.filter((ele) => ele === target).length;
+    }, 0);
 }
