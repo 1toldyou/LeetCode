b71543bc2fc4ee775e959240c23c1acc58b6ea9a
1toldyou <86680163+1toldyou@users.noreply.github.com>
2025-03-31 00:43:43 -0700

    optimized 1926

--- LeetCode/1926.ts
+++ LeetCode/1926.ts
@@ -10,10 +10,6 @@ function nearestExit(maze: string[][], entrance: number[]): number {
         return !(coord.x < 0 || coord.x >= maze.length || coord.y < 0 || coord.y >= maze[0].length || isWall(coord));
     };
     const isExit = (coord) => {
-        if (!canGo(coord)){
-            // console.log(coord, "cannot be exit as it cannot be reached");
-            return false;
-        }
         return coord.x === 0 || coord.x === maze.length-1 || coord.y === 0 || coord.y === maze[0].length-1;
     };
 
@@ -22,43 +18,30 @@ function nearestExit(maze: string[][], entrance: number[]): number {
         visited.push((new Array(maze[i].length)).fill(false));
     };
     visited[entrance[0]][entrance[1]] = true;
-    let emptyCell = count2D(maze, "."); // including entrance
-    let visitedCell = 1;
 
     let currentLevel = [toCoord(entrance)];
     let steps = 1;
-    while (currentLevel.length > 0){
+    while (currentLevel.length > 0){ // otherwise options are exhuasted
         let nextLevel = [];
-        console.log("currentLevel", currentLevel)
         for (let coord of currentLevel){
-            let adjacents = [
-                {x: coord.x, y: coord.y+1}, 
-                {x: coord.x, y: coord.y-1},
-                {x: coord.x+1, y: coord.y},
-                {x: coord.x-1, y: coord.y},
-            ].filter((next) => {
+            for (let next of [{x: coord.x, y: coord.y+1}, {x: coord.x, y: coord.y-1}, {x: coord.x+1, y: coord.y}, {x: coord.x-1, y: coord.y}]){
                 if (!canGo(next)){ // will check if it out of bounds
-                    return false;
+                    continue;
                 }
 
                 if (visited[next.x][next.y] === true){
-                    return false;
+                    continue;
                 }
 
-                return true;
-            });
-
-            for (let next of adjacents){
                 if (isExit(next)){
                     return steps;
                 }
                 nextLevel.push(next);
-                visited[next.x][next.y] = true
+                visited[next.x][next.y] = true;
             }
         }
         currentLevel = nextLevel;
         steps++;
-        visitedCell = count2D(visited, true);
     }
     return -1;
 };
@@ -73,10 +56,4 @@ function toCoord(arr: number[]): Coord {
         x: arr[0],
         y: arr[1],
     }
-}
-
-function count2D(twoDArr: any[][], target): number{
-    return twoDArr.reduce((accu, row) => {
-        return accu += row.filter((ele) => ele === target).length;
-    }, 0);
 }
