9c9d9d647dd7d4ccce625547e67777af2c2c8cbc
1toldyou <86680163+1toldyou@users.noreply.github.com>
2025-09-25 16:32:54 -0700

    Update 205.ts

--- LeetCode/205.ts
+++ LeetCode/205.ts
@@ -1,15 +1,22 @@
 function isIsomorphic(s: string, t: string): boolean {
     let dict = new Map();
+    let exist = new Set();
     for (let i = 0; i < s.length; i++){
         let a = s[i];
         let b = t[i];
         if (!dict[a]){
+            if (exist.has(b)){
+                return false;
+            }
+
             dict[a] = b;
         }
 
         if (dict[a] !== b){
             return false;
         }
+
+        exist.add(b);
     }
     return true;
 };
