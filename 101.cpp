7be1176749a1659659116c83f90c05dc2bf446bc
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-01-19 23:34:37 -0800

    Implement isSymmetric function for binary tree

---
+++ 101.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return recur(root->left, root-> right);
+    }
+
+    bool recur(TreeNode* a, TreeNode* b){
+        if (!a && !b){
+            return true;
+        }
+        if (!a || !b){
+            return false;
+        }
+
+        if (a->val != b->val){
+            return false;
+        }
+
+        return recur(a->left, b->right) && recur(a->right, b->left);
+    }
+};
