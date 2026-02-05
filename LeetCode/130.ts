7192197478f90db0e45b0a474e02aa2f3830406d
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-02-05 01:17:36 -0800

    Create 130.ts

---
+++ LeetCode/130.ts
@@ -0,0 +1,156 @@
+/**
+ Do not return anything, modify board in-place instead.
+ */
+type Coord = {
+    r: number
+}
+
+function solve(board: string[][]): void {
+    const n = board[0].length;
+    const m = board.length;
+
+    const dX = [1, -1, 0, 0];
+    const dY = [0, 0, 1, -1];
+
+    const makeGrid = () => {
+        let grid = new Array(m);
+        for (let i = 0; i < m; i++){
+            grid[i] = new Array(n).fill(false);
+        }
+        return grid;
+    }
+
+    let bordered = makeGrid();
+
+    const isFillable = (sR: number, sC: number) => {
+        if (board[sR][sC] == "X" || bordered[sR][sC]) return false;
+
+        let visited = makeGrid();
+        let current = [[sR, sC]];
+        while (current.length > 0){
+            // console.log("current", current);
+            // console.log("visited", visited)
+            let next = [];
+            for (let [r, c] of current){
+                // console.log(r, c);
+                
+
+                visited[r][c] = true;
+                if (board[r][c] === "X"){
+                    continue;
+                }
+
+                for (let i = 0; i < 4; i++){
+                    let nC = c+dX[i];
+                    let nR = r+dY[i];
+                    if (nC < 0 || nC == n || nR < 0 || nR == m){
+                        continue;
+                    }
+                    if (bordered[r][c]){
+                        return false;
+                    }
+                    if (visited[nR][nC] === true){
+                        // console.log("nR", nR, "nC", nC, "visited")
+                        continue;
+                    }
+                    if (board[nR][nC] === "X"){
+                        // console.log("nR", nR, "nC", nC, "is-X")
+                        continue;
+                    }
+                    if (nC == 0 || nC == n-1 || nR == 0 || nR == m-1){
+                        // console.log("nR", nR, "nC", nC, "is-border")
+                        return false;
+                    }
+                    
+                    next.push([nR, nC]);
+                }
+            }
+            current = next;
+        }
+        return true;
+    };
+
+    const fill = (sR: number, sC: number) => {
+        let current = [[sR, sC]];
+        board[sR][sC] = "X";
+        while (current.length > 0){
+            // console.log("current", current);
+            let next = [];
+            for (let [r, c] of current){
+                console.log(r, c);
+                if (board[r][c] === "X"){
+                    continue;
+                }
+                for (let i = 0; i < 4; i++){
+                    let nC = c+dX[i];
+                    let nR = r+dY[i];
+                    if (nC == 0 || nC == n-1 || nR == 0 || nR == m-1){
+                        continue;
+                    }
+                    if (board[nR][nC] === "X"){
+                        continue;
+                    }
+                    board[nR][nC] = "X";
+                    next.push([nR, nC]);
+                }
+            }
+            current = next;
+        }
+    };
+
+    const markBordered = (sR: number, sC: number) => {
+        if (board[sR][sC] === "X" || bordered[sR][sC]){
+            return;
+        }
+
+        let visited = makeGrid();
+        let current = [[sR, sC]];
+        visited[sR][sC] = true;
+        bordered[sR][sC] = true;
+        while (current.length > 0){
+            // console.log("current", current);
+            let next = [];
+            for (let [r, c] of current){
+                for (let i = 0; i < 4; i++){
+                    let nR = r + dY[i];
+                    let nC = c + dX[i];
+
+                    if (nC < 0 || nC >= n || nR < 0 || nR >= m){
+                        continue;
+                    }
+                    if (board[nR][nC] === "X"){
+                        continue;
+                    }
+                    if (visited[nR][nC] || bordered[nR][nC]){
+                        continue;
+                    }
+
+                    visited[nR][nC] = true;
+                    bordered[nR][nC] = true;
+                    next.push([nR, nC]);
+                }
+            }
+            current = next;
+        }
+    }
+
+    for (let i = 0; i < n; i++){
+        markBordered(0, i);
+        markBordered(m-1, i);
+    }
+    for (let i = 0; i < m; i++){
+        markBordered(i, 0);
+        markBordered(i, n-1);
+    }
+
+    console.log("markBordered");
+
+    for (let sR = 1; sR < m-1; sR++){
+        for (let sC = 1; sC < n-1; sC++){
+            if (isFillable(sR, sC)){
+                // console.log(sR, sC, "fillable");
+                fill(sR, sC)
+            }
+        }
+    }
+};
