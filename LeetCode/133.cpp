1104ab6c883bb6884970cc0cdb65cd1a163a2dc7
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-02-09 14:52:38 -0800

    Create 133.cpp

---
+++ LeetCode/133.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    map<int, Node*> nodes;
+    map<int, bool> processed;
+
+    void recur(Node* node){
+        int key = node->val;
+        if (processed.count(key) == 1 && processed[key] == true){
+            return;
+        }
+    
+        if (nodes.count(key) == 0){
+            nodes[key] = new Node(key);
+        }
+        
+        for (Node* neighbor : node->neighbors){
+            int neighborKey = neighbor->val;
+            if (nodes.count(neighborKey) == 0){
+                nodes[neighborKey] = new Node(neighborKey);
+            }
+            nodes[key]->neighbors.push_back(nodes[neighborKey]);
+        }
+        processed[key] = true;
+
+        for (Node* neighbor : node->neighbors){
+            int neighborKey = neighbor->val;
+            recur(neighbor);
+        }
+    }
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node){
+            return nullptr;
+        }
+
+        recur(node);
+        return nodes[node->val];
+    }
+};
