37e5d314a53c2e9718194965620b70e1922bd7a1
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-02-09 16:34:34 -0800

    Create 62.cpp

---
+++ LeetCode/62.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector dp(m, vector(n, 0));
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (dp[i][j] == 1){
+                    continue;
+                }
+
+                if (i > 0){
+                    dp[i][j] += dp[i-1][j];
+                }
+                if (j > 0){
+                    dp[i][j] += dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
