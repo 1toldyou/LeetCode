948b6859f26e99434efc6aeda0448313d72f5665
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-03-02 22:20:39 -0800

    207.py

--- LeetCode/207.py
+++ LeetCode/207.py
@@ -7,27 +7,21 @@ class Solution:
 
         checked = [False] * numCourses
 
-        for i, preReqs in enumerate(courses):
-            print(i, preReqs)
-            def dfs(course: int, visited: List[bool]) -> bool:
-                # print("finding dependency of", course, visited)
-                if checked[course]:
-                    return True
-                if len(courses[course]) == 0:
-                    return True
-                if visited[course]:
-                    # print("cycle detected at", course)
-                    return False
-                visited[course] = True
-                if len(courses[course]) == 1:
-                    if not dfs(courses[course][0], visited):
-                        return False
-                else:
-                    for preReq in courses[course]:
-                        if not dfs(preReq, copy.deepcopy(visited)):
-                            return False
-                checked[course] = True
+        def dfs(course: int, visited: List[bool]) -> bool:
+            # print("finding dependency of", course, visited)
+            if checked[course]:
                 return True
+            if visited[course]:
+                # print("cycle detected at", course)
+                return False
+            visited[course] = True
+            for preReq in courses[course]:
+                if not dfs(preReq, visited):
+                    return False
+            checked[course] = True
+            return True
+
+        for i in range(numCourses):
             if not dfs(i, [False] * numCourses):
                 return False
 
