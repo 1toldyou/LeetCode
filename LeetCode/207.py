09a1fd5b07f7cd6fdc11f6b31766dcf4df769e4f
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-03-02 22:16:36 -0800

    207.py

--- LeetCode/207.py
+++ LeetCode/207.py
@@ -3,26 +3,32 @@ class Solution:
         courses = [[] for i in range(numCourses)]
         for course, preReq in prerequisites:
             courses[course].append(preReq)
-        print(courses)
+        # print(courses)
+
+        checked = [False] * numCourses
 
         for i, preReqs in enumerate(courses):
             print(i, preReqs)
-            for preReq in preReqs:
-                visited = [False] * numCourses
-                def dfs(course: int) -> bool:
-                    print("finding dependency of", course)
-                    if len(courses[course]) == 0:
-                        return True
-                    if visited[course]:
-                        print("cycle detected at", course)
-                        return False
-                    visited[course] = True
-                    for preReq in courses[course]:
-                        if not dfs(preReq):
-                            return False
+            def dfs(course: int, visited: List[bool]) -> bool:
+                # print("finding dependency of", course, visited)
+                if checked[course]:
                     return True
-                print("starting at", preReq)
-                if not dfs(preReq):
+                if len(courses[course]) == 0:
+                    return True
+                if visited[course]:
+                    # print("cycle detected at", course)
                     return False
+                visited[course] = True
+                if len(courses[course]) == 1:
+                    if not dfs(courses[course][0], visited):
+                        return False
+                else:
+                    for preReq in courses[course]:
+                        if not dfs(preReq, copy.deepcopy(visited)):
+                            return False
+                checked[course] = True
+                return True
+            if not dfs(i, [False] * numCourses):
+                return False
 
         return True
