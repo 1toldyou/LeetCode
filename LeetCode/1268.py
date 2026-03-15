9b9c0e51176f848109443cf6a91c6a491db3dbb1
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-03-15 14:17:45 -0700

    1268.py

--- LeetCode/1268.py
+++ LeetCode/1268.py
@@ -1,12 +1,13 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        A = ord("a")
         trie = [None] * 27
 
         def add(node: dict, word: str):
             if word == "":
                 node[0] = True
                 return
-            idx = ord(word[0]) - ord("a") + 1
+            idx = ord(word[0]) - A + 1
             if node[idx] is None:
                 node[idx] = [None] * 27
 
@@ -14,39 +15,30 @@ class Solution:
             
         for product in products:
             add(trie, product)
-        # print(trie)
 
-        ans = []
-        level = []
+        ans = [[] for i in range(len(searchWord))]
+        ansIdx = 0
         seen = ""
         def search(node: dict, path: str):
-            # print("search()", path)
-            nonlocal level
-            if len(level) >= 3:
+            if len(ans[ansIdx]) >= 3:
                 return
 
             if node[0] == True:
-                level.append(path)
+                ans[ansIdx].append(path)
 
             for i in range(1, 27):
                 if node[i] is None:
                     continue
-                search(node[i], path+chr(ord("a")+i-1))
+                search(node[i], path+chr(A+i-1))
 
         ptr = trie
-        for c in searchWord:
-            idx = ord(c) - ord("a") + 1
+        for i, c in enumerate(searchWord):
+            idx = ord(c) - A + 1
             if ptr[idx] is None:
                 break
+            ansIdx = i
             seen += c
             ptr = ptr[idx]
             search(ptr, seen)
-            ans.append(level)
-            level = []
-        if len(ans) < len(searchWord):
-            ans += [[] for i in range(len(searchWord)-len(ans))]
+
         return ans
-
-
-        
-        
