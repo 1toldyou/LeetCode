626be086d6dd22c92b7832a5731901d45f0b79c2
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-03-18 15:24:39 -0700

    5.py

---
+++ LeetCode/5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+
+        ans = s[0]
+
+        for i in range(1, n):
+            a = i
+            b = i
+            while a-1>= 0 and b+1<n and s[a-1] == s[b+1]:
+                a -= 1
+                b += 1
+            # print(f"i={i} a={a} b={b}")
+            if len(s[a:b+1]) > len(ans):
+                ans = s[a:b+1]
+
+        for i in range(1, n):
+            a = i-1
+            b = i
+            if s[a] != s[b]:
+                continue
+            while a-1>= 0 and b+1<n and s[a-1] == s[b+1]:
+                a -= 1
+                b += 1
+            # print(f"i={i} a={a} b={b}")
+            if len(s[a:b+1]) > len(ans):
+                ans = s[a:b+1]
+
+        return ans 
