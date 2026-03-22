d163adc875ed3126913794f4829e5c1e75370958
1toldyou <86680163+1toldyou@users.noreply.github.com>
2026-03-22 11:35:17 -0700

    91.py

--- LeetCode/91.py
+++ LeetCode/91.py
@@ -3,29 +3,20 @@ class Solution:
         n = len(s)
 
         # viability check
-        for i in range(n):
-            if int(s[i]) == 0:
-                if i == 0:
-                    return 0
-                elif int(s[i-1]) > 2:
-                    return 0
+        if s[0] == "0":
+            return 0
         
 
-        dp = [0] * n
-        for i in range(n):
-            num = int(s[i])
-            if num == 0:
-                dp[i] = 0
-            elif num <= 2 and i < n-1 and int(s[i+1]) <= 6:
-                dp[i] = 2
-            else:
-                dp[i] = 1
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            one = int(s[i-1]) 
+            two = int(s[i-2:i])
+            # print(f"i={i} one={one} two={two}")
+            if one != 0:
+                dp[i] += dp[i-1]
+            if two >= 10 and two <= 26:
+                dp[i] += dp[i-2]
 
-        print(dp)
-        freqs = Counter(dp)
-        twos = freqs.get(2, 0)
-        zeros = freqs.get(0, 0)
-
-        return 1 + max(0, (twos - zeros*2))
-
-        
+        return dp[n]       
